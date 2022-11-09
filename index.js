3e03e6e8b40244b2ce27c9a3f13b4cc41873a901
yogendraft21 <yogendra311204@gmail.com>
2022-11-09 14:59:35 +0530

    navbar login

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// navbar--------------------------------
+$('#showFilePanel').click(function(event) {
+    if ($('.notification-container').hasClass('dismiss')) {
+      $('.notification-container').removeClass('dismiss').addClass('selected').show();
+    }
+    event.preventDefault();
+  });
+  
+  $('#closeFilePanel').click(function(event) {
+    if ($('.notification-container').hasClass('selected')) {
+      $('.notification-container').removeClass('selected').addClass('dismiss');
+    }
+    event.preventDefault();
+  });
+// navbar-----------------------------
+
 let slidePostion  = 0;
 const slides = document.getElementsByClassName("carousel_item");
 const totalslides = slides.length;
