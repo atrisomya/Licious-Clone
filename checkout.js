9faf2ec21872711438ebb2125cd88cc1bd506cc0
Mhendrapratap <mahendrapratp354@gmail.com>
2022-11-13 11:43:17 +0530

    pushing checkout changes

--- checkout.js
+++ checkout.js
@@ -1,1 +1,7 @@
 let current_user = JSON.parse(localStorage.getItem('currentUser'));
+let user_name = document.getElementById("showFilePanel");
+user_name.innerText = current_user;
+user_name.style.color='#d11243';
+
+
+
